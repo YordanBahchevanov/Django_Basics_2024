ccdc0e176fbac846b271f828301aa421600a5163
DanDan93SB <yordanbah9376@gmail.com>
2024-09-29 15:45:07 +0300

    feat: Adding Delete and Edit buttons for the posts. Adding Search functionality in dashboard to filter posts.

--- forumApp/forumApp/posts/views.py
+++ forumApp/forumApp/posts/views.py
@@ -3,12 +3,11 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from forumApp.posts.forms import PostForm
+from forumApp.posts.forms import PostForm, PostCreateForm, PostDeleteForm, SearchForm
 from forumApp.posts.models import Post
 
 
 def index(request):
-
     context = {
         "my_form": "",
     }
@@ -17,16 +16,24 @@ def index(request):
 
 
 def dashboard(request):
+    form = SearchForm(request.GET)
+    posts = Post.objects.all()
+
+    if request.method == "GET":
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            posts = posts.filter(title__icontains=query)
 
     context = {
-        "posts": Post.objects.all(),
+        "posts": posts,
+        "form": form,
     }
 
     return render(request, 'posts/dashboard.html', context)
 
 
 def add_post(request):
-    form = PostForm(request.POST or None)
+    form = PostCreateForm(request.POST or None)
 
     if request.method == "POST":
         if form.is_valid():
@@ -40,17 +47,37 @@ def add_post(request):
     return render(request, 'posts/add-post.html', context)
 
 
+def details_page(request, pk: int):
+    post = Post.objects.get(pk=pk)
+
+    context = {
+        "post": post,
+    }
+
+    return render(request, 'posts/details-post.html', context)
 
 
+def edit_post(request, pk: int):
+    post = Post.objects.get(pk=pk)
+
+    context = {
+        "post": post,
+    }
+
+    return render(request, 'posts/edit-post.html', context)
 
 
+def delete_post(request, pk: int):
+    post = Post.objects.get(pk=pk)
+    form = PostDeleteForm(instance=post)
 
+    if request.method == "POST":
+        post.delete()
+        return redirect('dash')
 
+    context = {
+        "form": form,
+        "post": post,
+    }
 
-
-
-
-
-
-
-
+    return render(request, 'posts/delete-template.html', context)
